53f3e17b9d3fa66894ee636adeace3c162a242ec
MDSUMONALI-1 <mdsumonaliceng@gmail.com>
2021-09-13 16:19:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>yes, i Edit</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://facebook.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React&sijdah for Allah
         </a>
       </header>
     </div>
